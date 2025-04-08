81a6c19462e926e243e7657fe6830fa957acac7b
franlo42 <franlozmell@gmail.com>
2025-04-09 00:15:41 +0200

    Ej extra 2 Sem 3->calcula_max

--- calcula_max_incompleto.cu
+++ calcula_max_incompleto.cu
@@ -27,26 +27,41 @@ void calcula_max(int *A, int *sal)
  *sal=maximo;
 }
 
- 
- int main() {
+__global__ void max_gpu_v1(int *A, int *sal){//1 bloque por columna
+  
+  int tid,temp;
+  tid = threadIdx.x + blockIdx.x * blockDim.x;
+  temp = A[tid];
 
- int i,j;
+  for(int i=1;i<N-1;i++){
+    if(A[tid + blockIdx.x *i] > temp){
+      temp=A[tid + blockIdx.x *i];
+    }
+  }
+  sal[blockIdx.x]=temp;
+}
+
+__global__ void max_gpu_v2(int *A, int *sal){
+
+}
+ 
+int main() {
+
+  int i,j;
  
  
   int *A = (int *) malloc( N*N*sizeof(int) );
   int salcpu;
 
 
- //rellenar matriz de enteros en CPU
+  //rellenar matriz de enteros en CPU
   for (i=0;i<N;i++)
-    for(j=0;j<N;j++)
-   {
+    for(j=0;j<N;j++){
       A[i+N*j]=rand()% 1000;
-     
     }
-Print_matrix(A);
-calcula_max(A,&salcpu);
-printf(" \n El maximo calculado en cpu es %d ",salcpu);
+  Print_matrix(A);
+  calcula_max(A,&salcpu);
+  printf("\nEl maximo calculado en cpu es %d ",salcpu);
 
 
 
@@ -57,14 +72,41 @@ printf(" \n El maximo calculado en cpu es %d ",salcpu);
 //Comienzo parte GPU
 
   int *sal= (int *)malloc(N*sizeof(int) ); //variable para copiar resultado parcial de gpu a cpu
+  int *sal2= (int *)malloc(N*sizeof(int) ); //variable para copiar resultado parcial de gpu a cpu
 //variables para gpu
   int *dev_A;
   int *dev_sal;
+  int *dev_sal2;
+
+  //Rservar espacio en GPU para vectores que usaremos
+  cudaMalloc((void **) &dev_A, N*N*sizeof(int));
+  cudaMalloc((void **) &dev_sal, N*sizeof(int));
   
+  cudaMalloc((void **) &dev_sal2, N*sizeof(int));
+  
+  //Llevar a GPU la matriz A ya llena
+  cudaMemcpy( dev_A, A, N*N*sizeof(int), cudaMemcpyHostToDevice);
+
+  //llamada kernel v1
+  max_gpu_v1<<<N,1>>>(dev_A, dev_sal);
+
+  //Traer de gpu el resultado
+  cudaMemcpy( sal, dev_sal, N*sizeof(int), cudaMemcpyDeviceToHost);
+
+
+  int sol=sal[0];
+  for(int i=1;i<N-1;i++){
+    if(sal[i]>sol){
+      sol=sal[i];
+    }
+  }
+  printf("\nEl maximo calculado en gpuV1 es %d ",sol);
 
   free(A);
-free(sal);
+  free(sal);
  
-  }
+  cudaFree(dev_A);
+  cudaFree(dev_sal);
+}
 	
 	
